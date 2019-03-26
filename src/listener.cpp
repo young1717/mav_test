b27f3631b52e892eff755b07274a51d648c3e5df
young1717 <18829234902@163.com>
2019-03-26 22:18:46 +0800

    state of uav

--- src/listener.cpp
+++ src/listener.cpp
@@ -76,6 +76,15 @@ int main(int argc, char **argv)
 		if(uav_state->guided)
 			ROS_INFO("Change the mode to GUIDED successfully!");
 	}
+	
+	auto home_client = n.serviceClient<mavros_msgs::CommandHome>("/mavros/cmd/set_home");
+	mavros_msgs::CommandHome home_srv;
+
+	auto takeoff_client = n.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/takeoff");
+	mavros_msgs::CommandTOL takeoff_srv;
+
+	auto land_client = n.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/land");
+	mavros_msgs::CommandTOL land_srv;
 
 	uint8_t task_state = 0;
 	while(true){
@@ -90,7 +99,7 @@ int main(int argc, char **argv)
 			{
 				if(uav_state->armed){
 					arm_srv.request.value = false;
-					if(arm_client.call(arm_src))
+					if(arm_client.call(arm_srv))
 						ROS_INFO("DisArm!");
 					else
 						ROS_WARN("DisArm didn't finish!");
@@ -118,16 +127,9 @@ int main(int argc, char **argv)
 			{
 				if(uav_state->armed && uav_state->guided)
 				{
-					Command(SET_HOME_AS_CUR_POSITION, 0, NONE);
-					Global_Pos home_p = uas.get_curpos();
-					uas.update_home_pos(home_p);
-					
-					task_state = TASK_DO_TAKEOFF;
-					/*static Global_Pos origin = uas.get_home_pos();//起点经纬度
-					for(int i = 0;i < NUM_TARGET;i++)
-					{
-						way_gpoint[i] = get_latlon_from_xy(origin, way_lpoint[i]*100);
-					}*/
+					home_srv.request.current_gps = true;
+					if(home_client.call(home_srv)&&home_srv.response.success)
+						task_state = TASK_DO_TAKEOFF;
 				}
 				printf("TASK_DO_SET_HOME\r\n");
 				break;
@@ -136,23 +138,15 @@ int main(int argc, char **argv)
 			{
 				if(uav_state->armed && uav_state->guided)
 				{
-					Command(TAKEOFF, ALT_CONSTANT, NONE);
-					gettimeofday(&tv, &tz);
-					uas.update_task_first_time(tv);
-					task_state = TASK_WAIT_FOR_STABLE;
+					if(takeoff_client.call(takeoff_srv)&&takeoff_srv.response.success)
+						task_state = TASK_WAIT_FOR_STABLE;
 				}
 				printf("TASK_DO_TAKEOFF\r\n");
 				break;
 			}
 			case TASK_WAIT_FOR_STABLE:
 			{
-				gettimeofday(&cur_time, &tz);
-				first_time = uas.get_task_first_time();
-				float delta_ms = float(cur_time.tv_sec - first_time.tv_sec)*1e3 + float(cur_time.tv_usec - first_time.tv_usec)/1e3;
-				if(delta_ms > 1e4)
-				{
-					task_state = TASK_DO_NAV_CONTROL;
-				}
+				task_state = TASK_DO_NAV_CONTROL;
 				printf("TASK_WAIT_FOR_STABLE\r\n");
 				break;
 			}
@@ -160,7 +154,7 @@ int main(int argc, char **argv)
 			{
 				if(uav_state->armed && uav_state->guided)
 				{
-					left_right_flag = formation();
+					//left_right_flag = formation();
 					/*if(finish)
 					{
 						task_state = TASK_DO_LAND;
@@ -176,8 +170,8 @@ int main(int argc, char **argv)
 			{
 				if(uav_state->armed && uav_state->guided)
 				{
-					Command(LAND, 0, NONE);
-					task_state = TASK_FINISH;
+					if(land_client.call(land_srv)&&land_srv.response.success)
+						task_state = TASK_FINISH;
 				}
 				printf("TASK_DO_LAND\r\n");
 				break;
